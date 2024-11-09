c0f1bf51d33c7a3031c549f4c4ca82bc4bdaf6d3
Najeeb Sulaiman <beejan003@gmail.com>
2024-11-09 21:43:38 +0000

    First configuration of the environment and tables

---
+++ includes/constants.js
@@ -0,0 +1,9 @@
+const PROJECT_ID = 'primeval-melody-314601';
+const REGION = 'region-eu';
+const DATASET_ID = 'jlr_vehicle_data';
+
+module_exports = {
+    PROJECT_ID,
+    REGION,
+    DATASET_ID
+};
